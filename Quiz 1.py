1d03d40c145e1bb7e1fea563f2a4cc7528442484
Sarah Stephenson <123026466+sstephenson01@users.noreply.github.com>
2023-01-27 11:43:43 -0500

    Add files via upload

---
+++ Quiz 1.py
@@ -0,0 +1,18 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#Problem 4
+2+(5-3)**5*27/(4*3)
+74.0
+#Problem 5
+print("be not afraid."n/
+      
+SyntaxError: '(' was never closed
+print("be not afraid.",n/"some are born grat, some achieve greatness, and others have greatness thrust upon them")
+      
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    print("be not afraid.",n/"some are born grat, some achieve greatness, and others have greatness thrust upon them")
+NameError: name 'n' is not defined
+print("be not afraid,nl some are born great, some achieve greatness, and others have greatness thrust upon them")
+      
+be not afraid,nl some are born great, some achieve greatness, and others have greatness thrust upon them
